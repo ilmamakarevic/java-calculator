9eb79cbc34b100f3e6eaaa0e45e1eecf6486cf36
Ilma Makarevic <makarevic85@gmail.com>
2025-03-10 23:47:08 +0100

    Added new functions

--- calculator/src/calculator/Caluclations.java
+++ calculator/src/calculator/Caluclations.java
@@ -1,6 +1,6 @@
 package calculator;
 
-public class Calculations {
+public class Caluclations {
 	
 	public int add(int a, int b) {
 		System.out.print(a + " + " + b + " = " );
@@ -25,5 +25,32 @@ public class Calculations {
 			System.out.print(a + " / " + b + " = ");
 			return a/b;
 		}
+	}
+
+	public int remainder(int a, int b) {
+		System.out.print(a + " % " + b + " = ");
+		return a%b;
+	}
+	
+	public int exponentiation(int a, int b) {
+		int result=1;
+		
+		if(b==0) {
+			return 1;
+		}
+		else if(b==1) {
+			return a;
+		}
+		else {
+			for(int i = 1; i<=b; i++) {
+				result = result*a;
+			}
+			return result;
+		}
+	}
+	
+	public double squareRoot(double a) {
+		System.out.print("Square root of " + a );
+		return Math.sqrt(a);
 	}
 }
--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Calculations clc = new Calculations();
+		Caluclations  clc = new Caluclations();
 		
 		System.out.println(clc.add(-12, 3));
 		System.out.println(clc.subtract(3, 5));
 		System.out.println(clc.multiply(0, 3));
 		System.out.println(clc.divide(6, -3));
-		
+		System.out.println(clc.remainder(5, 2));
+		System.out.println(clc.exponentiation(5, 2));
+		System.out.println(clc.squareRoot(5));
 	}
 
 }
